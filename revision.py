55369cd00ef57f687460ec85f230396b0e4bc5b0
Feli-St <22fstaubach@wakatipu.school.nz>
2025-03-19 14:56:08 +1300

    more progress

--- revision.py
+++ revision.py
@@ -1,2 +1,13 @@
+numbers = []
+
 for i in range(5):
+    print("Enter a number:")
+    num = int(input(">"))
+    numbers.append(num)
+
+product = numbers[0]
+for i in range(len(numbers)-2):
+    print(f"{product} x {numbers[i+1]} = {product * numbers[i+1]}")
+    product = product * numbers[i+1]
+    
     
