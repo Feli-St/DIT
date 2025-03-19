f381c0b844134b347143eed95a3dd9aa228a761e
Feli-St <22fstaubach@wakatipu.school.nz>
2025-03-19 15:03:43 +1300

    input validation

--- revision.py
+++ revision.py
@@ -1,8 +1,20 @@
+def get_int():
+    while True:
+        try:
+            num = int(input(">"))
+            if num >= 2 and num <= 10:
+                return num
+            else:
+                print("Enter a number between 2 and 10")
+        except ValueError:
+            print("Enter a valid number")
+
+
 numbers = []
 
 for i in range(5):
     print("Enter a number:")
-    num = int(input(">"))
+    num = get_int()
     numbers.append(num)
 
 product = numbers[0]
